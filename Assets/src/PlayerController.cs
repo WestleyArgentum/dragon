008fc0cd6a9785cd0de677e52fa3093e9f3a1548
WestleyArgentum <WestleyArgentum@gmail.com>
2015-04-18 18:17:48 -0400

    jump action

--- Assets/src/PlayerController.cs
+++ Assets/src/PlayerController.cs
@@ -4,27 +4,48 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	public float maxSpeed = 10f;
-	bool facingRight = true;
+	public float jumpForce = 400f;
 
-	Animator animator;
+	[HideInInspector] public bool facingRight = true;
+	[HideInInspector] public bool jump = false;
 
-	// Use this for initialization
-	void Start () {
-		animator = GetComponent<Animator> ();
+	private bool grounded = false;
+	public Transform groundCheck;
+
+	private Animator animator;
+	private Rigidbody2D body;
+
+	void Awake () {
+		animator = GetComponent<Animator>();
+		body = GetComponent<Rigidbody2D>();
 	}
 	
-	// Update is called once per frame
+	void Update() {
+		int layer = LayerMask.NameToLayer ("Ground");
+		grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << layer);
+		
+		if (Input.GetButtonDown("Jump") && grounded) {
+			jump = true;
+		}
+	}
+
 	void FixedUpdate () {
 		float move = Input.GetAxis("Horizontal");
 
 		animator.SetFloat("speed", Mathf.Abs(move));
 
-		rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
+		GetComponent<Rigidbody2D>().velocity = new Vector2(move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
 
 		if (move > 0 && !facingRight)
 			Flip();
 		else if (move <= 0 && facingRight)
 			Flip();
+
+		if (jump) {
+			jump = false;
+			animator.SetTrigger("Jump");
+			body.AddForce(new Vector2(0f, jumpForce));
+		}
 	}
 
 	void Flip () {
