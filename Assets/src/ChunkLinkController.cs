a0a5d0fce71b0b61f72bb98bdadecabcc96716ac
WestleyArgentum <WestleyArgentum@gmail.com>
2015-04-19 17:00:13 -0400

    first pass at procedually generating world chunks

---
+++ Assets/src/ChunkLinkController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChunkLinkController : MonoBehaviour {
+	public enum linkTypes {
+		Left,
+		Right
+	};
+
+	public linkTypes linkType;
+
+	private Transform player;
+	private Vector3 chunkOffset;
+	private Object linked = null;
+
+	void Awake() {
+		chunkOffset = transform.parent.position - transform.position;
+
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+
+	void Update() {
+		if (linked == null && (transform.position - player.position).sqrMagnitude < 2 * 2) {
+			GameObject newChunk = (GameObject)Instantiate((Object)Resources.Load("world-chunk-1"), transform.position, Quaternion.identity);
+
+			// find a compatible link
+			linkTypes lookingForLink = MatchingLinkType();
+			ChunkLinkController[] newLinks = newChunk.GetComponentsInChildren<ChunkLinkController>();
+
+			foreach (ChunkLinkController link in newLinks) {
+				if (link.linkType == lookingForLink) {
+					LinkNewChunk(link);
+				}
+			}
+		}
+	}
+
+	void LinkNewChunk(ChunkLinkController newLink) {
+		linked = newLink;
+		newLink.linked = this;
+
+		newLink.transform.parent.position += newLink.chunkOffset;
+	}
+
+	linkTypes MatchingLinkType() {
+		return linkType == linkTypes.Left ? linkTypes.Right : linkTypes.Left;
+	}
+}
